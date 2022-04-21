1663a882c62c62d05a762fb10d98b246ef60a6ef
Aakash Mehta <aakashmehta880@gmail.com>
2022-04-22 01:31:26 +0530

    question-2 squares

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+function App() {
+  return (
+    <div id="main">
+      <div id="square1"></div>
+      <div id="square2">
+        <div id="circle"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
